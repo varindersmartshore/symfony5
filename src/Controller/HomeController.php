8da9df97008828dbd88513e8a2aef961e80e1983
Varinder Kaur <varinder@smartshore.nl>
2021-08-11 14:25:11 +0530

    Initial Setup

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+// src/Controller/HomeController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    public function home(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Home number: '.$number.'</body></html>'
+        );
+    }
+
+    /**
+    * @Route("/about")
+    */
+    public function about()
+    {
+        return $this->render('about.html.twig');
+    }
+}
